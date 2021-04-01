c86826f441d6e91dfead64b174bcc8953a6d11f7
hI <jktmobile6637@gmail.com>
2021-04-02 02:22:45 +0900

    Update Docker CI/CI Test 2

--- src/main/java/com/teckstudy/book/controller/HelloController.java
+++ src/main/java/com/teckstudy/book/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Docker World! CI/CD Update Test!";
+        return "Hello, Docker World! CI/CD Update Test!2";
     }
 }
