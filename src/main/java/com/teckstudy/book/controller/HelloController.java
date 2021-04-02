daa5604ad48b96ca676301a89398154f841901e1
hI <jktmobile6637@gmail.com>
2021-04-02 23:14:37 +0900

    Update github webhook test

--- src/main/java/com/teckstudy/book/controller/HelloController.java
+++ src/main/java/com/teckstudy/book/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Docker World! CI/CD Update Test! 4";
+        return "Hello, Docker World! CI/CD Update Test! 5";
     }
 
 }
