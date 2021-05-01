e5a5781bbcfdaaa13f9bb5c161f46f721b92de10
hI <jktmobile6637@gmail.com>
2021-05-01 17:52:46 +0900

    ADD custom 상속

--- src/main/java/com/teckstudy/book/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teckstudy.book.repository;
-
-
-import com.teckstudy.book.entity.BookOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<BookOrder, Long> {
-}
