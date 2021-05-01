2089211eeb97c62caf52e06feb4aa752eb4c050a
hI <jktmobile6637@gmail.com>
2021-05-01 17:54:03 +0900

    ADD 아이디 주문 조회 서비스 구현

--- src/main/java/com/teckstudy/book/order/service/OrderService.java
+++ src/main/java/com/teckstudy/book/order/service/OrderService.java
@@ -1,12 +1,22 @@
 package com.teckstudy.book.order.service;
 
+import com.teckstudy.book.order.domain.dto.OrderBookResponseDto;
 import com.teckstudy.book.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    public List<OrderBookResponseDto> findOrderListById(Long memberSn){
+        return orderRepository.findOrderListById(memberSn);
+    }
+
+
 }
