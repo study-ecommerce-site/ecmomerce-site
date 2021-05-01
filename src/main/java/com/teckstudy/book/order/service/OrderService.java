d72e678479192e6f7d8000e053f92c34b5a16bfc
hI <jktmobile6637@gmail.com>
2021-05-01 18:40:22 +0900

    ADD 주문 결과 서비스(수정 필요)

--- src/main/java/com/teckstudy/book/order/service/OrderService.java
+++ src/main/java/com/teckstudy/book/order/service/OrderService.java
@@ -1,11 +1,19 @@
 package com.teckstudy.book.order.service;
 
+import com.teckstudy.book.entity.BookOrder;
 import com.teckstudy.book.order.domain.dto.OrderBookResponseDto;
+import com.teckstudy.book.order.domain.dto.OrderResultResponseDto;
 import com.teckstudy.book.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -16,6 +24,18 @@ public class OrderService {
 
     public List<OrderBookResponseDto> findOrderListById(Long memberSn){
         return orderRepository.findOrderListById(memberSn);
+    }
+
+    @Transactional
+    public List<OrderResultResponseDto> order(List<BookOrder> orderInfoList){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        List<OrderResultResponseDto> orderResultResponseDtoList =
+                orderRepository.saveAll(orderInfoList).stream().map(p -> modelMapper.map(p, OrderResultResponseDto.class))
+                .collect(Collectors.toList());
+
+        return orderResultResponseDtoList;
     }
 
 
