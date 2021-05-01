e47bc146db7e99eb6877620c0473a04f91eb5320
hI <jktmobile6637@gmail.com>
2021-05-01 18:40:04 +0900

    ADD 주문 결과 Repo

--- src/main/java/com/teckstudy/book/order/repository/OrderCustomRepository.java
+++ src/main/java/com/teckstudy/book/order/repository/OrderCustomRepository.java
@@ -1,12 +1,9 @@
 package com.teckstudy.book.order.repository;
 
-import com.teckstudy.book.entity.BookOrder;
 import com.teckstudy.book.order.domain.dto.OrderBookResponseDto;
-import com.teckstudy.book.order.domain.dto.OrderResultResponseDto;
 
 import java.util.List;
 
 public interface OrderCustomRepository {
     List<OrderBookResponseDto> findOrderListById(Long memberSn);
-
 }
