76c9ef52f3e06b85999bee3a76043658ecb93862
prodo-developer <wjdals300@gmail.com>
2021-10-13 00:42:45 +0900

    refactor : 전시관리 시퀀스 추가

--- src/main/java/com/teckstudy/book/entity/Exhibition.java
+++ src/main/java/com/teckstudy/book/entity/Exhibition.java
@@ -1,17 +1,30 @@
 package com.teckstudy.book.entity;
 
 import com.teckstudy.book.entity.enums.ExhibitionType;
+import com.teckstudy.book.entity.enums.YesNoStatus;
+import lombok.*;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 
-public class Exhibition {
+@Entity
+@Getter @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(
+        name = "EXHIBITION_SEQ_GENERATOR",
+        sequenceName = "EXHIBITION_SEQ", // 매핑할 데이터베이스 시퀀스 이름
+        initialValue = 10000001,
+        allocationSize = 1)
+public class Exhibition extends BaseEntity{
 
     // 전시코너코드
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "EXHIBITION_SEQ_GENERATOR")
     private Long exhibition_sn;
 
     // 전시코너 사용여부
-    private boolean use_yn;
+    private YesNoStatus use_yn;
 
     // 전시코너명
     private String name;
@@ -21,7 +34,7 @@ public class Exhibition {
     private ExhibitionType exhibitionType;
 
     // 전시기간 설정
-    private boolean date_yn;
+    private YesNoStatus date_yn;
 
     // 전시코너 이미지
     private String image;
