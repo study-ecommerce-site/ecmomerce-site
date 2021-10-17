722992f7e7da8cc22a5d5868863ecf0f1e3d9deb
prodo-developer <wjdals300@gmail.com>
2021-10-17 21:33:09 +0900

    feat : 컨텐츠 타입 레포지토리 추기

---
+++ src/main/java/com/teckstudy/book/product/repository/ContentsTypeRepository.java
@@ -0,0 +1,7 @@
+package com.teckstudy.book.product.repository;
+
+import com.teckstudy.book.entity.ContentsType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentsTypeRepository extends JpaRepository<ContentsType, Long> {
+}
