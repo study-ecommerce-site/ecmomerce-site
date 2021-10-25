a859b2a3fafe446d106179cd86d12199900a30ed
prodo-developer <wjdals300@gmail.com>
2021-10-25 15:56:59 +0900

    refactor : 단일업로드 로직 수정 및 멀티파일 업로드 개발

--- src/main/java/com/teckstudy/book/lib/common/UploadResult.java
+++ src/main/java/com/teckstudy/book/lib/common/UploadResult.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,5 +14,5 @@ import lombok.NoArgsConstructor;
 public class UploadResult {
 
     private int code; // 100 성공
-    private String path;
+    private List<String> path;
 }
--- src/main/java/com/teckstudy/book/product/controller/FileController.java
+++ src/main/java/com/teckstudy/book/product/controller/FileController.java
@@ -1,19 +1,44 @@
 package com.teckstudy.book.product.controller;
 
+import antlr.collections.List;
 import com.teckstudy.book.lib.common.UploadResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @RestController
 public class FileController {
 
+    /**
+     * 단일파일 업로드
+     * @param file
+     * @return
+     */
     @PostMapping(value = "/upload")
     public UploadResult upload(@RequestParam(value = "file") MultipartFile file) {
         return UploadResult.builder()
                 .code(100)
-                .path("/files/"+file.getOriginalFilename())
+//                .path(List.of("/files/"+file.getOriginalFilename()))
+                .path(Arrays.asList("/files/"+file.getOriginalFilename()))
+                .build();
+    }
+
+    /**
+     * 여러개 멀티파일 업로드
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "/uploads")
+    public UploadResult uploads(@RequestParam(value = "files") MultipartFile[] file) {
+        return UploadResult.builder()
+                .code(100)
+                .path(Arrays.stream(file).map(f -> "/files/"+f.getOriginalFilename())
+                .collect(Collectors.toList()))
                 .build();
     }
 }
