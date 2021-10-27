030d98baa38746213b6d455b1a77ba025f064ead
prodo-developer <wjdals300@gmail.com>
2021-10-28 00:15:02 +0900

    refactor : 파일업로드 검증 로직 구현

--- src/main/java/com/teckstudy/book/lib/common/util/controller/FileController.java
+++ src/main/java/com/teckstudy/book/lib/common/util/controller/FileController.java
@@ -1,16 +1,22 @@
-package com.teckstudy.book.product.controller;
+package com.teckstudy.book.lib.common.util.controller;
 
 import com.teckstudy.book.lib.common.UploadResult;
+import com.teckstudy.book.lib.common.util.service.FileService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @RestController
 public class FileController {
+
+    private final FileService fileService;
 
     /**
      * 단일파일 업로드
@@ -18,7 +24,8 @@ public class FileController {
      * @return
      */
     @PostMapping(value = "/upload")
-    public UploadResult upload(@RequestParam(value = "file") MultipartFile file) {
+    public UploadResult upload(@RequestParam(value = "file") MultipartFile file) throws IOException {
+        fileService.save(file);
         return UploadResult.builder()
                 .code(100)
 //                .path(List.of("/files/"+file.getOriginalFilename()))
@@ -35,7 +42,15 @@ public class FileController {
     public UploadResult uploads(@RequestParam(value = "files") MultipartFile[] file) {
         return UploadResult.builder()
                 .code(100)
-                .path(Arrays.stream(file).map(f -> "/files/"+f.getOriginalFilename())
+                .path(Arrays.stream(file).map(f -> {
+                    try {
+                        fileService.save(f);
+                        return "/files/"+f.getOriginalFilename();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return "error to save : "+f.getOriginalFilename();
+                    }
+                })
                 .collect(Collectors.toList()))
                 .build();
     }
---
+++ src/main/java/com/teckstudy/book/lib/common/util/service/FileService.java
@@ -0,0 +1,40 @@
+package com.teckstudy.book.lib.common.util.service;
+
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+
+    @Setter
+    private Path root;
+
+    public FileService() {
+        // Path.of는 9부터 지원
+        this.root = Paths.get("upload-dir");
+
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void init() throws IOException {
+        if(Files.exists(root)) {
+            Files.createDirectories(root);
+        }
+    }
+
+    public void save(MultipartFile file) throws IOException {
+        file.transferTo(root.resolve(file.getOriginalFilename()));
+    }
+
+
+}
