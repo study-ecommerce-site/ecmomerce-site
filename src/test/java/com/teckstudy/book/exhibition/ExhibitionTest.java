7236a14ce7a4f6a1d80586e6b7b9574ba3c09cd2
prodo-developer <wjdals300@gmail.com>
2021-10-28 12:37:09 +0900

    refactor : 파일업로드 다른 테스트 경로로 분리

--- src/test/java/com/teckstudy/book/exhibition/ExhibitionTest.java
+++ src/test/java/com/teckstudy/book/exhibition/ExhibitionTest.java
@@ -41,26 +41,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Transactional
 public class ExhibitionTest extends WebIntegrationTest {
 
-    @Autowired
-    private FileService fileService;
-
-    @BeforeEach
-    void before() throws IOException {
-        // 다이렉트 사용방법
-//        File rootDir = new File("src/test/resources/files");
-//        fileService.setRoot(rootDir.toPath());
-        // 랜덤하게 사용
-        Path rootDir = new File("src/test/resources/files").toPath();
-        Path root = rootDir.resolve("" + Math.abs(new Random().nextLong()));
-        fileService.setRoot(root);
-        fileService.init();
-    }
-
-    @AfterEach
-    void after() {
-
-    }
-
     @Autowired // 또는 자바 표준 스택 @PersistenceContext 최신버전부터 @Autowired 지원 됨
     EntityManager em;
 
@@ -176,41 +156,4 @@ public class ExhibitionTest extends WebIntegrationTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> new BoValidation().multiFileUpload(bytes));
     }
-
-    @Test
-    @DisplayName("서버에서 넘겨준 파일 업로드시 10MB를 초과하면 Exception을 발생시킨다.")
-    public void fileServerUploadTest() {
-
-        HttpEntity request = makeRequest(new ClassPathResource("testimage1.jpg"));
-
-        ResponseEntity<UploadResult> response = client.postForEntity(url("/upload"), request, UploadResult.class);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("/files/testimage1.jpg", response.getBody().getPath().get(0));
-    }
-
-    @Test
-    @DisplayName("2개 이상의 파일을 업로드 한다.")
-    public void fileServerUploadsTest() {
-        HttpEntity request = makeRequest(
-                new ClassPathResource("testimage1.jpg"),
-                new ClassPathResource("testimage2.jpg")
-        );
-
-        ResponseEntity<UploadResult> response = client.postForEntity(url("/uploads"), request, UploadResult.class);
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("/files/testimage1.jpg", response.getBody().getPath().get(0));
-        assertEquals("/files/testimage2.jpg", response.getBody().getPath().get(1));
-    }
-
-    private HttpEntity makeRequest(Resource... resources) {
-        MultiValueMap form = new LinkedMultiValueMap();
-        if(resources.length == 1) {
-            form.add("file", resources[0]);
-        } else {
-            Arrays.stream(resources).forEach(res -> form.add("files", res));
-        }
-        HttpEntity request = new HttpEntity(form, null);
-        return request;
-    }
-
 }
